c08998cccf157663f12c777957e1818075041315
Comwp ODG <comwp6@gmail.com>
2026-03-16 21:10:27 -0500

    Reverted order of operations for display name of a star without clicking as part of testing

--- app.js
+++ app.js
@@ -765,7 +765,7 @@
 
                 overlayCtx.globalAlpha = textFadeAmt <= 1 ? textFadeAmt : 1;
 
-                var dispName = hideENINames ? (s.fedName) : (s.fedName ?? s.name);
+                var dispName = hideENINames ? (s.fedName) : (s.name??s.fedName);
 
                 overlayCtx.fillText(
                     dispName,
